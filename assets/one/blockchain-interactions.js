d1acd399d7bae50002d5c9a69f260f4e760b8e80
Vladyslav <jason.soc@ukr.net>
2018-07-04 12:00:22 +0200

    hardware added

--- assets/one/blockchain-interactions.js
+++ assets/one/blockchain-interactions.js
@@ -4,6 +4,7 @@ const neo = require("../../backend/blockchain.js");
 const account = require("../../backend/config.js");
 const util = require("../../backend/util.js");
 const tools = require("../../backend/tools.js");
+
 const Config = require('electron-config');
 const config = new Config();
 
@@ -39,14 +40,12 @@ btn.addEventListener('click', () => {
     }
 });
 
-neon.api.neoscan.getBalance("TestNet", account.address).then((res) => {
+neon.api.neoscan.getBalance("TestNet", config.get("address")).then((res) => {
     neo_assets = res.assets["NEO"].balance;
     gas_assets = res.assets["GAS"].balance;
     neo_assets = (Math.floor(neo_assets * 100) / 100);
     gas_assets = (Math.floor(gas_assets * 100) / 100);
     neo_c.innerHTML = neo_assets;
     gas_c.innerHTML = gas_assets;
-    
+
 });
-
-
---
+++ assets/one/hardware-interactions.js
@@ -0,0 +1,16 @@
+
+const os = require("os");
+
+const ram_free = document.getElementById("ram_free");
+const ram_total = document.getElementById("ram_total");
+const cpu_cores = document.getElementById("cpu");
+
+const mbTotal = ((os.totalmem()) / 1048576);
+const cpuCount = os.cpus().length;
+ram_total.innerHTML = mbTotal;
+cpu_cores.innerHTML = cpuCount;
+
+const intervalObj = setInterval(() => {
+    let mbFree = ((os.freemem()) / 1048576);
+    ram_free.innerHTML = mbFree;
+}, 500);
